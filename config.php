373ec189bed9a205e109c2b8f51ee61a31aac897
Dong Jun Lee <47773765+djleedenny@users.noreply.github.com>
2019-04-17 21:29:49 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
-
-/**
-  * Configuration for database connection
-  *
-  */
-
-$host       = "localhost";
-$username   = "root";
-$password   = "1234";
-$dbname     = "world"; // will use later
+$host       = "mercury.nottingham.edu.my";
+$username   = "hfynm5_hfynm5";
+$password   = "hannatakcomel";
+$dbname     = "hfynm5_world"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
